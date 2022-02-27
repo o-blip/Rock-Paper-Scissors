4ea87a713460ef2cc39d366e3749323bc6737677
Piero <dolibiroli@gmail.com>
2022-02-27 12:01:51 -0600

    added borders to selected choices

--- code.js
+++ code.js
@@ -8,8 +8,6 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-
-
     switch (playerSelection) {
         // First switch determine player's throw
         // 1: player wins, 0 computer wins, 2 means tie
@@ -43,13 +41,13 @@ function playRound(playerSelection, computerSelection) {
 }
 
 
-function game(playerSelection) {
+function game(playerSelection, computerSelection) {
     // First select the scores from the html, then just add 1 to each scores
     const playerScore = document.querySelector("#playerScore");
     const computerScore = document.querySelector("#computerScore");
     const winner = document.querySelector("#winner");
 
-    if (playerScore.textContent === '5' || computerScore.textContent === '5'){
+    if (playerScore.textContent === '5' || computerScore.textContent === '5') {
         winner.textContent = '';
         playerScore.textContent = '0';
         computerScore.textContent = '0';
@@ -57,13 +55,13 @@ function game(playerSelection) {
         return;
     }
 
-    if (playRound(playerSelection, computerPlay()) === 1) { // 1 means player won
+    if (playRound(playerSelection, computerSelection) === 1) { // 1 means player won
         winner.textContent = '';
         playerScore.textContent++;
-    } else if (playRound(playerSelection, computerPlay()) === 0) { // 0 means computer won, dont keep track of ties
+    } else if (playRound(playerSelection, computerSelection)=== 0) { // 0 means computer won, dont keep track of ties
         winner.textContent = '';
         computerScore.textContent++;
-    } else{
+    } else {
         winner.textContent = 'Tie!'
     }
     if (playerScore.textContent === '5') {
@@ -74,9 +72,25 @@ function game(playerSelection) {
     }
 }
 
+function showSelection(buttonID, computerID) {
+    const playerSelection = document.querySelector('#' + buttonID);
+    const computerSelection = document.querySelector('#'+computerID);
+    const allChoices = document.querySelectorAll('input');
+    allChoices.forEach((choice) => choice.classList.remove('selected-throw-player','selected-throw-computer'));
+
+
+    playerSelection.classList.add('selected-throw-player');
+    computerSelection.classList.add('selected-throw-computer');
+}
 
 
 const buttons = document.querySelectorAll('input');
 buttons.forEach((button) => {
-    button.addEventListener('click', () => game(button.id))
+    button.addEventListener('click', () => {
+        const computerSelection = computerPlay();
+        game(button.id, computerSelection);
+        showSelection(button.id, computerSelection);
+
+    })
+
 });
